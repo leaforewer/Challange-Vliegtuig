8e2980d0265d334750a27025c3c5ed1d7f3fd93d
Ahmet Erdogan <329188@student.mboutrecht.nl>
2019-11-07 13:20:40 +0100

    challange klaar

---
+++ Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    public float speed = 1000f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Rotate(1, speed*Time.deltaTime, 1);
+        }
+    
+
+}
+
